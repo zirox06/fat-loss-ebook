3a432719ba12d5fa0268c1b0f9350d698ba2b9eb
zirox06 <116922314+zirox06@users.noreply.github.com>
2025-05-18 10:32:25 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -27,4 +27,8 @@ def download():
     return send_from_directory(UPLOAD_FOLDER, 'Fat_Loss_eBook.pdf', as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
